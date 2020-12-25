11e40ae9d972d58198277371d6e23d3cd8974a74
Daksh19084 <70843957+Daksh19084@users.noreply.github.com>
2020-12-25 14:54:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var BG1,BG2,end;
+var Boss2,Hydra;
+var Monster1,monster2,Gun;
+function preload(){
+Boss2= loadImage('Images/boss2.png')
+Hydra= loadImage('Images/Hydra.jpg')
+Gun= loadImage("Images/Gun.png")
+Monster1= loadImage("Images/monster1.png")
+monster2= loadImage("Images/Monster2.png")
+}
+var BG3,Bg4,end;
+function setup(){
+create
+var canvas = createCanvas(1200,400);
+
+
+}
+function draw(){
+
+BG1.display();
+BG2.display();
+end.display();
+Boss2.display();
+Hydra.display();
+monster2.display();
+Monster1.display();
+Gun.display();
+
+}
